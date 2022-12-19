b22279e176a605d95ae1a591cc529827218c9e25
wadelucky <44307100+wadelucky@users.noreply.github.com>
2022-12-19 18:31:15 -0500

    Update run.sh
    Changed some configs.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,18 +1,17 @@
 EXPERIMENT_PATH="output/unsup_pretrain"
 mkdir -p $EXPERIMENT_PATH
-DATASET_PATH=~/Datasets/imagenet/
+DATASET_PATH=datasets/imagenet/
 DATASET=imagenet
 
-python -m torch.distributed.launch \
---nproc_per_node=8 \
+python -m torch.distributed.run \
+--nproc_per_node=1 \
 --nnodes=1 \
---node_rank=0 \
 --master_addr="127.0.0.1" \
 --master_port=40000 \
 flr/main.py \
 --data_path $DATASET_PATH \
 --nmb_crops 2 6 \
---size_crops 224 96 \
+--size_crops 32 16 \
 --min_scale_crops 0.14 0.05 \
 --max_scale_crops 1. 0.14 \
 --use_pil_blur false \
@@ -24,17 +23,19 @@ flr/main.py \
 --nmb_prototypes 3000 \
 --queue_length 0 \
 --epoch_queue_starts 15 \
---epochs 400 \
---batch_size 128 \
+--epochs 5 \
+--batch_size 32 \
 --base_lr 4.8 \
 --final_lr 0.0048 \
 --freeze_prototypes_niters 313 \
 --wd 0.000001 \
---warmup_epochs 10 \
+--warmup_epochs 0 \
 --start_warmup 0.3 \
 --dist_url "tcp://127.0.0.1:40000" \
 --arch resnet50 \
 --use_fp16 true \
 --sync_bn apex \
 --dataset $DATASET \
---dump_path $EXPERIMENT_PATH
+--dump_path $EXPERIMENT_PATH \
+--tensorboard-log-dir tensorboard
+--syncbn_process_group_size 1
